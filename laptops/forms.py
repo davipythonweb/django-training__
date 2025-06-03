f9eeed6aec8385172668e3bffda9f9ad7823b6bd
davipythonweb <galaxys3tab32@gmail.com>
2025-06-03 00:54:36 -0300

    edting forms.py in laptop

--- laptops/forms.py
+++ laptops/forms.py
@@ -12,14 +12,14 @@ class laptopModelForm(forms.ModelForm):
 
   def clean_value(self): # validaçao de formulario (valor do laptopro)
     value = self.cleaned_data.get('value')
-    if value < 20000:
-      self.add_error('value', 'Valor mínimo de laptopro deve ser de R$20.000.')
+    if value < 2000:
+      self.add_error('value', 'Valor mínimo de laptop deve ser de R$2000.')
     return value
 
   def clean_factory_year(self): # validaçao do ano de fabricação
     factory_year = self.cleaned_data.get('factory_year')
-    if factory_year < 1975:
-      self.add_error('factory_year', 'Não é possivel cadastrar laptopros fabricados antes de 1975.')
+    if factory_year < 2005:
+      self.add_error('factory_year', 'Não é possivel cadastrar laptops fabricados antes de 2005.')
     return factory_year
 
 
